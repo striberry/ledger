c9f3bbd59ddd3fe262b3ceca1aada38d75f5fe01
Stribes <stribes@tutanota.de>
2026-05-01 12:55:58 +0200

    chore: update to v1.0.0 (#1)
    * chore: add ci and release workflows and AUR PKGBUILD
    
    Signed-off-by: striberry <stribes@tutanota.de>
    
    * chore: update package version
    
    Signed-off-by: striberry <stribes@tutanota.de>
    
    * fix: clippy print_literal warnings
    
    Signed-off-by: striberry <stribes@tutanota.de>
    
    * fix: add guard to verify tag is on main
    
    Signed-off-by: striberry <stribes@tutanota.de>
    
    ---------
    
    Signed-off-by: striberry <stribes@tutanota.de>

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -12,7 +12,7 @@ pub fn run(ledger: &Ledger) -> Result<()> {
 
     let task_w = tasks.iter().map(|t| t.text.len()).max().unwrap_or(4).max(4);
 
-    println!("{:>4}  {}", "ID", "Task");
+    println!("{:>4}  Task", "ID");
     println!("{}", "─".repeat(4 + 2 + task_w));
 
     for task in tasks {
